35745095f1bce38bbf214a530c2c118d98e3218e
Manu Meixell <misternu@gmail.com>
2025-03-05 20:27:46 -0800

    Chapter 4 Lesson 2

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,7 +1,33 @@
 """ splitting and typing markdown blocks """
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    """ type enum for blocks """
+    PARAGRAPH = 1
+    HEADING = 2
+    CODE = 3
+    QUOTE = 4
+    UNORDERED_LIST = 5
+    ORDERED_LIST = 6
+
 def markdown_to_blocks(markdown):
     """ takes a markdown formatted string and returns blocks of text """
     strings = markdown.split("\n\n")
     strings = list(map(str.strip, strings))
     strings = list(filter(lambda block: not len(block) == 0, strings))
     return strings
+
+def block_to_block_type(block):
+    """ identify type of block """
+    if re.match(r'\A#{1,6} .*', block, re.DOTALL):
+        return BlockType.HEADING
+    if re.match(r'\A```.*```', block, re.DOTALL):
+        return BlockType.CODE
+    if re.match(r'(^>.*)+', block, re.DOTALL):
+        return BlockType.QUOTE
+    if re.match(r'(^- .*)+', block, re.DOTALL):
+        return BlockType.UNORDERED_LIST
+    if re.match(r'(^\d+\. .*)+', block, re.DOTALL):
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,6 @@
 """ test the splitting and typing of markdown blocks """
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestMarkdownToHTML(unittest.TestCase):
     """ tests for transforming markdown to blocks to nodes to html """
@@ -28,6 +28,21 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_block_types(self):
+        """ test identification of block types """
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
 
 if __name__ == "__main__":
     unittest.main()
